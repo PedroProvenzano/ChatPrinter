54325fde2b24e601b6ba48c4169615713a538330
Pedro Provenzano <pedroprov0@gmail.com>
2021-01-22 15:08:33 -0300

    Correccion en orden de chat y print de mensajes

--- public/script.js
+++ public/script.js
@@ -3,13 +3,23 @@ const contenedorChat = document.getElementById('contenedor-chat');
 
 
 socket.on('newMsg', message => {
+    let username = mayusmaker(message.username);
     let div = document.createElement('div');
     div.setAttribute("class", "mensaje");
     let pUno = document.createElement('p');
-    pUno.innerText = message.username;
+    pUno.innerText = username;
     let pDos = document.createElement('p');
+    pDos.setAttribute('class', 'msgCont');
     pDos.innerText = message.msg;
     div.appendChild(pUno);
     div.appendChild(pDos);
-    contenedorChat.appendChild(div);
+    contenedorChat.insertBefore(div, contenedorChat.firstChild);
 });
+
+
+function mayusmaker(name)
+{
+    let username = name.slice(1);
+    let firstLetter = name[0].toUpperCase();
+    return firstLetter + username;
+}
